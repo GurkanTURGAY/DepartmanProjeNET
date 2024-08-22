0c92791bd68e4dcf2bbcaf5766b88678a9464bec
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-08-22 17:29:41 +0300

    Add project files.

---
+++ Departman/Controllers/DefaultController.cs
@@ -0,0 +1,49 @@
+﻿using Departman.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Departman.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+
+            var degerler = c.Birims.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public IActionResult YeniBirim()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult YeniBirim(Birim b)
+        {
+            c.Birims.Add(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult BirimSil(int id)
+        {
+            var dep = c.Birims.Find(id);
+            c.Birims.Remove(dep);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult BirimGetir(int id)
+        {
+            var depart = c.Birims.Find(id);
+            return View("BirimGetir", depart);
+        }
+        public IActionResult BirimGuncelle(Birim d)
+        {
+            var dep = c.Birims.Find(d.BirimID);
+            dep.BirimAd = d.BirimAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Departman/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Departman.Models
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-99SLPDA\\SQLEXPRESS; database=Birimdb; integrated security=true; TrustServerCertificate=True;");
+        }
+
+        public DbSet<Birim> Birims { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+
+    }
+}
