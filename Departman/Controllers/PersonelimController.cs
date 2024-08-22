2675497b391837be3da353a0c928960435b6cb76
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-08-22 17:51:32 +0300

    birimler ve personeller tablolarının tamamlanması

---
+++ Departman/Controllers/PersonelimController.cs
@@ -0,0 +1,16 @@
+﻿using Departman.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Departman.Controllers
+{
+    public class PersonelimController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            var degerler = c.Personels.Include(x => x.Birim).ToList();
+            return View(degerler);
+        }
+    }
+}
